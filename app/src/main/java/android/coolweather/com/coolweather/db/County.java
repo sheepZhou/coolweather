011b4b3f2c6d0e6938e354d094bd4051f5bb16d5
sheep <17601448642@163.com>
2018-04-18 13:45:02 +0800

    First commit

---
+++ app/src/main/java/android/coolweather/com/coolweather/db/County.java
@@ -0,0 +1,46 @@
+package android.coolweather.com.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 10139 on 2018/4/18.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;//县的名字
+    private String weatherId;//县所对应的天气id
+    private int cityId;//记录当前县所属市的id
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
---
+++ app/src/main/java/android/coolweather/com/coolweather/db/Province.java
@@ -0,0 +1,38 @@
+package android.coolweather.com.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 10139 on 2018/4/18.
+ * 存放省
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;//省的名字
+    private int provinceCode;//省的代号
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
